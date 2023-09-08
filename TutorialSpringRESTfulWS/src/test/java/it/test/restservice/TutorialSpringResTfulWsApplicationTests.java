2c26cca43831a9224605619180b32f692b4e4cdd
Ale1990gh <32124847+Ale1990gh@users.noreply.github.com>
2023-09-08 16:34:04 +0200

    commit.

---
+++ TutorialSpringRESTfulWS/src/test/java/it/test/restservice/TutorialSpringResTfulWsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.test.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialSpringResTfulWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
